0169783cf8eeb085fe7a7b588acf48e440a968f0
JUHIE <75068056+juhiechandra@users.noreply.github.com>
2023-03-07 16:37:42 +0530

    simulation running when start is pressed

--- life-game/src/App.tsx
+++ life-game/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import produce from 'immer';
 
 // import logo from './logo.svg';
@@ -19,44 +19,56 @@ const App: React.FC = () => {
 
   const [running, setRunning] = useState(false);
 
-  return (
-    <>
-      <button onClick={() => { setRunning(!running); }}>
-        {running ? "Stop" : "Start"}
-      </button>
+  const runningRef = useRef(running);
+  runningRef.current = running
 
-      <div style={
-        {
-          display: 'grid',
-          gridTemplateColumns: `repeat(${numCols}, 20px)`
-        }
-      }>
-        {/* <h1>Life Game</h1> */}
-        {
-          grid.map((rows, i) =>
-            rows.map((col, k) =>
-              <div key={`${i}-${k}`}
+  const runSimulation = useCallback(() => {
+    if (!running) {
+      return;
+    }
+    // simulate
+    setTimeout(runSimulation, 1000);
+  }, []);
 
-                onClick={() => {
-                  const newGrid = produce(grid, gridCopy => { gridCopy[i][k] = grid[i][k] ? 0 : 1; })
-                  setGrid(newGrid);
 
-                }}
+    return (
+      <>
+        <button onClick={() => { setRunning(!running); }}>
+          {running ? "Stop" : "Start"}
+        </button>
 
-                style={{
-                  width: 20,
-                  height: 20,
-                  backgroundColor: grid[i][k] ? 'pink' : undefined,
-                  border: 'solid 1px black'
-                }} />
+        <div style={
+          {
+            display: 'grid',
+            gridTemplateColumns: `repeat(${numCols}, 20px)`
+          }
+        }>
+          {/* <h1>Life Game</h1> */}
+          {
+            grid.map((rows, i) =>
+              rows.map((col, k) =>
+                <div key={`${i}-${k}`}
+
+                  onClick={() => {
+                    const newGrid = produce(grid, gridCopy => { gridCopy[i][k] = grid[i][k] ? 0 : 1; })
+                    setGrid(newGrid);
+
+                  }}
+
+                  style={{
+                    width: 20,
+                    height: 20,
+                    backgroundColor: grid[i][k] ? 'pink' : undefined,
+                    border: 'solid 1px black'
+                  }} />
+              )
             )
-          )
 
-        }
+          }
 
-      </div>
-    </>
-  )
-}
+        </div>
+      </>
+    )
+  }
 
-export default App;
+  export default App;
