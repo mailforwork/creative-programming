5a1241ef559a2ec31998cf2958582d657e2b526a
JUHIE <75068056+juhiechandra@users.noreply.github.com>
2023-03-07 16:58:20 +0530

    Added game rules and config js logics

--- life-game/src/App.tsx
+++ life-game/src/App.tsx
@@ -1,11 +1,23 @@
-import React, { useState, useCallback, useRef } from 'react';
-import produce from 'immer';
+import React, { useState, useCallback, useRef } from "react";
+import produce from "immer";
 
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 const numRows = 50;
 const numCols = 50;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+  // These are basically all neighbours around the cell
+];
 
 const App: React.FC = () => {
   const [grid, setGrid] = useState(() => {
@@ -15,60 +27,93 @@ const App: React.FC = () => {
     }
 
     return rows;
-  })
+  });
 
   const [running, setRunning] = useState(false);
 
   const runningRef = useRef(running);
-  runningRef.current = running
+  runningRef.current = running;
 
   const runSimulation = useCallback(() => {
-    if (!running) {
+    if (!runningRef.current) {
       return;
     }
+
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              // console.log(newI, newK);
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+                // console.log(neighbors);
+              }
+            });
+            // Update gridCopy
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+              console.log("dead"); // This is working
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+              console.log("alive"); // This is working
+            }
+          }
+        }
+      });
+    });
+
     // simulate
-    setTimeout(runSimulation, 1000);
+    setTimeout(runSimulation, 100);
   }, []);
 
-
-    return (
-      <>
-        <button onClick={() => { setRunning(!running); }}>
-          {running ? "Stop" : "Start"}
-        </button>
-
-        <div style={
-          {
-            display: 'grid',
-            gridTemplateColumns: `repeat(${numCols}, 20px)`
+  return (
+    <>
+      <button
+        onClick={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
           }
-        }>
-          {/* <h1>Life Game</h1> */}
-          {
-            grid.map((rows, i) =>
-              rows.map((col, k) =>
-                <div key={`${i}-${k}`}
+         
+        }}
+      >
+        {running ? "Stop" : "Start"}
+      </button>
 
-                  onClick={() => {
-                    const newGrid = produce(grid, gridCopy => { gridCopy[i][k] = grid[i][k] ? 0 : 1; })
-                    setGrid(newGrid);
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, 20px)`,
+        }}
+      >
+        {/* <h1>Life Game</h1> */}
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? "lime" : undefined,
+                border: "solid 1px black",
+              }}
+            />
+          ))
+        )}
+      </div>
+    </>
+  );
+};
 
-                  }}
-
-                  style={{
-                    width: 20,
-                    height: 20,
-                    backgroundColor: grid[i][k] ? 'pink' : undefined,
-                    border: 'solid 1px black'
-                  }} />
-              )
-            )
-
-          }
-
-        </div>
-      </>
-    )
-  }
-
-  export default App;
+export default App;
